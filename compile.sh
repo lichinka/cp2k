9b6a42c522cc93279a9cfcc6b03a7950a00a7628
Lucas Benedicic <benedicic@cscs.ch>
2016-04-08 13:42:56 +0200

    Script downloads the source before compilation.-

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-SRC_DIR="$( pwd )/cp2k-2.6.2"
+SRC_DIR="$( pwd )/cp2k-2.6"
+
+#
+# checkout the lastest source from the 2.6 branch
+#
+svn checkout http://svn.code.sf.net/p/cp2k/code/branches/cp2k-2_6-branch cp2k-src
+mv cp2k-src/cp2k ${SRC_DIR}
+rm -rf cp2k-src
 
 #
 # module setup for CP2K
@@ -30,7 +37,7 @@ AR       = ar -r
 CPPFLAGS =
 DFLAGS   = -D__FFTW3 -D__parallel -D__SCALAPACK -D__HAS_smm_dnn -D__ACC -D__DBCSR_ACC -D__PW_CUDA
 CFLAGS   = \$(DFLAGS) -fopenmp -fno-omit-frame-pointer -O3 -ffast-math
-FCFLAGS  = \$(DFLAGS) -fopenmp -mavx -funroll-loops -ffast-math -ftree-vectorize -ffree-form -ffree-line-length-512 -fno-omit-frame-pointer -std=f2003 -fimplicit-none -Werror=aliasing -Werror=ampersand -Werror=c-binding-type -Werror=intrinsic-shadow -Werror=intrinsics-std -Werror=line-truncation -Werror=tabs -Werror=realloc-lhs-all -Werror=target-lifetime -Werror=underflow -Werror=unused-but-set-variable -Werror=unused-variable -Werror=conversion -Wzerotrip
+FCFLAGS  = \$(DFLAGS) -fopenmp -mavx -funroll-loops -ffast-math -ftree-vectorize -ffree-form -ffree-line-length-512 -fno-omit-frame-pointer -std=f2003 -fimplicit-none -Werror=aliasing -Werror=ampersand -Werror=c-binding-type -Werror=intrinsic-shadow -Werror=intrinsics-std -Werror=line-truncation -Werror=tabs -Werror=realloc-lhs-all -Werror=target-lifetime -Werror=underflow -Werror=unused-but-set-variable -Werror=unused-variable -Werror=conversion -Wno-use-no-only -Wzerotrip
 LDFLAGS  = \$(FCFLAGS)
 NVFLAGS  = \$(DFLAGS) -arch sm_35
 LIBS     = -lfftw3 -lfftw3_threads -lcudart -lcufft -lcublas -lrt
@@ -41,4 +48,5 @@ EOF
 # compile
 #
 cd ${SRC_DIR}/makefiles
-make -j 1 ARCH=${ARCH_NAME} VERSION=${ARCH_VERS}
+make -j 4 ARCH=${ARCH_NAME} VERSION=${ARCH_VERS} && \
+echo "You may find the executables in <${SRC_DIR}/exe>"
