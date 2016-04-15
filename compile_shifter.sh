e4d6849bf2cd0df0bc191f455b977af8e0be4f85
Lucas Benedicic <benedicic@cscs.ch>
2016-04-15 11:14:11 +0200

    Compilation script for a container.-

--- compile_shifter.sh
+++ compile_shifter.sh
@@ -13,30 +13,26 @@ rm -rf cp2k-src
 # arch file
 #
 ARCH_NAME="Linux-x86-64-gfortran-cuda-custom"
-ARCH_VERS="sopt"
+ARCH_VERS="psmp"
 
 cat > ${SRC_DIR}/arch/${ARCH_NAME}.${ARCH_VERS} <<EOF
 # 
-# Serial version with CUDA double precision support
+# OpenMP+MPI+CUDA version with double precision support
 #
-PERL     = perl
-CC       = gcc
-CPP      = cpp
 NVCC     = nvcc -D__GNUC_MINOR__=6 -D__GNUC__=4
-FC       = gfortran
+CC       = gcc
+CPP      = g++ 
+FC       = gfortran 
 LD       = gfortran
 AR       = ar -r
 CPPFLAGS =
-DFLAGS   = -D__FFTW3 -D__parallel -D__SCALAPACK -D__ACC -D__DBCSR_ACC -D__PW_CUDA
+DFLAGS   = -D__FFTW3 -D__parallel -D__SCALAPACK -D__HAS_smm_dnn -D__ACC -D__DBCSR_ACC -D__PW_CUDA
 CFLAGS   = \$(DFLAGS) -fopenmp -fno-omit-frame-pointer -O3 -ffast-math
 FCFLAGS  = \$(DFLAGS) -fopenmp -mavx -funroll-loops -ffast-math -ftree-vectorize -ffree-form -ffree-line-length-512 -fno-omit-frame-pointer -std=f2003 -fimplicit-none -Werror=aliasing -Werror=ampersand -Werror=c-binding-type -Werror=intrinsic-shadow -Werror=intrinsics-std -Werror=line-truncation -Werror=tabs -Werror=realloc-lhs-all -Werror=target-lifetime -Werror=underflow -Werror=unused-but-set-variable -Werror=unused-variable -Werror=conversion -Wno-use-no-only -Wzerotrip
 LDFLAGS  = \$(FCFLAGS)
 NVFLAGS  = \$(DFLAGS) -arch sm_35
-LIBS     = -lfftw3 -lfftw3_threads -lcudart -lcufft -lcublas -lrt -pthread
-
-cuda_cublas.o: cuda_cublas.F
-	$(CPP) $(CPPFLAGSCU) $< > cuda_cublas.f90
-	$(FC) -c $(FCFLAGSCU) cuda_cublas.f90
+LIBS     = -lfftw3 -lfftw3_threads -lcudart -lcufft -lcublas -lrt
+LIBS    += ${SRC_DIR}/../libsmm_dnn_cray.gnu.a
 EOF
 
 #
